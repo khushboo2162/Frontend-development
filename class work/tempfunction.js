d7d1ef51bab5b9cf69c3ee7b9c2a7e42d831f4ba
khsuhboo2162 <khushbooagrawal8077@gmail.com>
2025-11-13 11:36:08 +0530

    Add js file

---
+++ class work/tempfunction.js
@@ -0,0 +1,17 @@
+function func(){
+    var temp=10;
+    console.log(temp);
+}
+
+func();
+
+var myfunc=function(){
+    var temp2=20;
+    console.log(temp2);
+}
+
+myfunc();
+
+var myfun2=()=> console.log(30);
+
+myfun2();
