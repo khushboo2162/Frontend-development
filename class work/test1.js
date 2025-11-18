5dca2e4fb14126d47948aea511842dc0baae8d67
khsuhboo2162 <khushbooagrawal8077@gmail.com>
2025-11-18 14:55:36 +0530

    Add js file

---
+++ class work/test1.js
@@ -0,0 +1,16 @@
+let student={
+    names:"John",
+    age:20,
+    grade:"A"
+}
+
+// for(let key in student){
+//     console.log(key+": "+student[key]);
+// }
+
+let fruits = ["apple", "banana", "cherry"];
+for (let fruit of fruits) {
+console.log(fruit);
+}
+
+// console.log(student.age);
